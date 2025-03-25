bdfc36aaa6304e77ee8e0daba14ed78b8887ce2b
timofeiemilian <emilianmail96@gmail.com>
2025-03-25 12:04:01 +0100

    Set routes.js

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,1 +1,33 @@
 // qui ci saranno le rotte
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/cv",
+    name: "cv",
+    component: () => import("../views/Cv.vue"),
+  },
+  {
+    path: "/cl",
+    name: "cl",
+    component: () => import("../views/Cl.vue"),
+  },
+  {
+    path: "/contatti",
+    name: "Contatti",
+    component: () => import("@/views/Contatti.vue"),
+  },
+];
+
+export default new VueRouter({
+  mode: "history", // rimuove l'# dall'URL
+  routes,
+});
