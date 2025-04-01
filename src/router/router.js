d3eeb74df601203a23834262b737e15bd31eb7eb
timofeiemilian <emilianmail96@gmail.com>
2025-04-01 10:54:17 +0200

    let's see

--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import CV from '@/views/CV.vue';
-import CL from '../views/CL.vue';
+import Cv from '../views/CV.vue';
+import Cl from '../views/CL.vue';
 import Contacts from '../views/Contacts.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/cv', component: CV },
-  { path: '/cl', component: CL },
+  { path: '/cv', component: Cv },
+  { path: '/cl', component: Cl },
   { path: '/contacts', component: Contacts },
 ];
 
