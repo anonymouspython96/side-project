a188b4a343386090592e72155bffa17e445fbf6c
timofeiemilian <emilianmail96@gmail.com>
2025-05-24 13:33:49 +0200

    rebuilding website!

---
+++ portfolio/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Hello the website it's in rebuilding!</h1>
+</template>
+
+<style scoped>
+h1 {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
